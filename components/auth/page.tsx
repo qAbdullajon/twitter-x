78dd9fc6f94c1581d452f18eee559c0c420244be
qAbdullajon <azikmelor7705@gmail.com>
2025-02-21 20:37:14 +0500

    first commit

--- components/auth/page.tsx
+++ components/auth/page.tsx
@@ -1,6 +1,10 @@
+import Image from "next/image"
+
 const Auth = () => {
   return (
-    <div>Auth</div>
+    <div className="grid grid-cols-2 gap-10 items-center h-screen">
+      {/* <Image src={'/images/x.svg'} width={450} height={450} alt="X" priority unoptimized /> */}
+    </div>
   )
 }
 
